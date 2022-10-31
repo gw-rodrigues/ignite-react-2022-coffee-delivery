9cd7c483393e05e07252d0c7c0e01c5e1557bf62
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-31 21:10:32 +0100

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { Trash } from 'phosphor-react'
+
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>hello world!</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
