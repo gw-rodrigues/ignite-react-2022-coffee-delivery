587bc6ea3ee3658697383a2117f4fc064fc4fe8f
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-31 20:50:53 +0100

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Trash } from 'phosphor-react'
+
 export function App() {
-  return <div>hello</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>hello world!</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
