2169c6c2a669b6093a177a0b0b0e4942f652c272
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-31 20:51:27 +0100

    style: add element colors

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: '#FFF',
+  background: '#FAFAFA',
+
+  'gray-100': '#F3F2F2',
+  'gray-200': '#EDEDED',
+  'gray-300': '#E6E5E5',
+  'gray-400': '#D7D5D5',
+
+  'gray-600': '#8D8686',
+  'gray-700': '#574F4D',
+  'gray-800': '#403937',
+  'gray-900': '#272221',
+
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-900': '#4B2995',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-500': '#DBAC2C',
+  'yellow-900': '#C47F17',
+}
