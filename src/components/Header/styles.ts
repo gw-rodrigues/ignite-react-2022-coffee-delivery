76cff3e5200764cddc7cf9a5a663e344a3f5a037
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-31 22:41:57 +0100

    style: header container

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 2rem 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  gap: 1rem;
+
+  img {
+    width: 85px;
+    height: auto;
+  }
+`
