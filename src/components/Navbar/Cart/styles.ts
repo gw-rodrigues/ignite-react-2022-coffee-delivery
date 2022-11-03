080b5725a7b8ff057a33002e67512e0191fe2704
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-03 11:52:57 +0100

    style: cart items count

--- src/components/Navbar/Cart/styles.ts
+++ src/components/Navbar/Cart/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const CartContainer = styled.button`
+  position: relative;
   background: ${(props) => props.theme['yellow-100']};
   transition: box-shadow 0.2s;
 
@@ -16,4 +17,25 @@ export const CartContainer = styled.button`
   svg {
     fill: ${(props) => props.theme['yellow-500']};
   }
+`
+
+export const CartItemsCount = styled.span`
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  width: 20px;
+  height: 20px;
+  background: ${(props) => props.theme['yellow-500']};
+  border-radius: 1000px;
+  padding: 0.15rem 0.15rem 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 0.75rem;
+  font-weight: 700;
+  line-height: 1.3;
+  letter-spacing: 0.06em;
+  color: ${(props) => props.theme.white};
 `
