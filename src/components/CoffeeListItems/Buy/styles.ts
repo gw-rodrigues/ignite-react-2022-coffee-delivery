cbf48c12623219d51700f955f8382cfb17165fbd
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-03 10:03:00 +0100

    style: buy card and price

---
+++ src/components/CoffeeListItems/Buy/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const BuyContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 1.4375rem;
+`
+
+export const PriceContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span:first-of-type {
+    font-size: 0.875rem;
+    line-height: 1rem;
+  }
+  span:last-of-type {
+    font-size: 1.5rem;
+    font-family: 'Baloo 2';
+    font-weight: 800;
+    line-height: 1.95rem;
+    margin-left: 0.2rem;
+  }
+`
