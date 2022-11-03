12f6e0a885d9048b9f0bb58767068fccd0cccef3
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-03 11:41:49 +0100

    feat: add types and coffee list values

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,8 +8,9 @@ export type TCoffee = {
   name: string
   description: string
   price: string
-  type: string[]
+  types: string[]
   quantity: number
+  image: string
 }
 
 export function Home() {
@@ -22,40 +23,45 @@ export function Home() {
         name: 'Expresso Tradicional',
         description: 'O tradicional café feito com água quente e grãos moídos',
         price: '9,90',
-        type: ['Tradicional'],
+        types: ['Tradicional'],
         quantity: 10,
+        image: 'Expresso.svg',
       },
       {
         id: 'Expresso_Americano',
         name: 'Expresso Americano',
         description: 'Expresso diluído, menos intenso que o tradicional',
         price: '9,90',
-        type: ['Tradicional'],
+        types: ['Tradicional'],
         quantity: 10,
+        image: 'Americano.svg',
       },
       {
         id: 'Expresso_Cremoso',
         name: 'Expresso Cremoso',
         description: 'Café expresso tradicional com espuma cremosa',
         price: '9,90',
-        type: ['Tradicional'],
+        types: ['Tradicional'],
         quantity: 10,
+        image: 'Expresso-Cremoso.svg',
       },
       {
         id: 'Expresso_Gelado',
         name: 'Expresso Gelado',
         description: 'Bebida preparada com café expresso e cubos de gelo',
         price: '9,90',
-        type: ['Tradicional', 'gelado'],
+        types: ['Tradicional', 'gelado'],
         quantity: 10,
+        image: 'Cafe-Gelado.svg',
       },
       {
         id: 'Café_com_Leite',
         name: 'Café com Leite',
         description: 'Meio a meio de expresso tradicional com leite vaporizado',
         price: '9,90',
-        type: ['Tradicional', 'com leite'],
+        types: ['Tradicional', 'com leite'],
         quantity: 10,
+        image: 'Cafe-com-Leite.svg',
       },
       {
         id: 'Latte',
@@ -63,8 +69,9 @@ export function Home() {
         description:
           'Uma dose de café expresso com o dobro de leite e espuma cremosa',
         price: '9,90',
-        type: ['Tradicional', 'com leite'],
+        types: ['Tradicional', 'com leite'],
         quantity: 10,
+        image: 'Latte.svg',
       },
       {
         id: 'Capuccino',
@@ -72,8 +79,9 @@ export function Home() {
         description:
           'Bebida com canela feita de doses iguais de café, leite e espuma',
         price: '9,90',
-        type: ['Tradicional', 'com leite'],
+        types: ['Tradicional', 'com leite'],
         quantity: 10,
+        image: 'Capuccino.svg',
       },
       {
         id: 'Macchiato',
@@ -81,8 +89,9 @@ export function Home() {
         description:
           'Café expresso misturado com um pouco de leite quente e espuma',
         price: '9,90',
-        type: ['Tradicional', 'com leite'],
+        types: ['Tradicional', 'com leite'],
         quantity: 10,
+        image: 'Macchiato.svg',
       },
       {
         id: 'Mocaccino',
@@ -90,8 +99,9 @@ export function Home() {
         description:
           'Café expresso com calda de chocolate, pouco leite e espuma',
         price: '9,90',
-        type: ['Tradicional', 'com leite'],
+        types: ['Tradicional', 'com leite'],
         quantity: 10,
+        image: 'Mocaccino.svg',
       },
       {
         id: 'Chocolate Quente',
@@ -99,8 +109,9 @@ export function Home() {
         description:
           'Bebida feita com chocolate dissolvido no leite quente e café',
         price: '9,90',
-        type: ['com leite', 'especial'],
+        types: ['com leite', 'especial'],
         quantity: 10,
+        image: 'Chocolate-Quente.svg',
       },
       {
         id: 'Cubano',
@@ -108,24 +119,27 @@ export function Home() {
         description:
           'Drink gelado de café expresso com rum, creme de leite e hortelã',
         price: '9,90',
-        type: ['gelado', 'alcoólico', 'especial'],
+        types: ['gelado', 'alcoólico', 'especial'],
         quantity: 10,
+        image: 'Cubano.svg',
       },
       {
         id: 'Havaiano',
         name: 'Havaiano',
         description: 'Bebida adocicada preparada com café e leite de coco',
         price: '9,90',
-        type: ['especial'],
+        types: ['especial'],
         quantity: 10,
+        image: 'Havaiano.svg',
       },
       {
         id: 'Árabe',
         name: 'Árabe',
         description: 'Bebida preparada com grãos de café árabe e especiarias',
         price: '9,90',
-        type: ['especial'],
+        types: ['especial'],
         quantity: 10,
+        image: 'Arabe.svg',
       },
       {
         id: 'Irlandês',
@@ -133,8 +147,9 @@ export function Home() {
         description:
           'Bebida a base de café, uísque irlandês, açúcar e chantilly',
         price: '9,90',
-        type: ['especial', 'alcoólico'],
+        types: ['especial', 'alcoólico'],
         quantity: 10,
+        image: 'Irlandes.svg',
       },
     ])
   }, [])
