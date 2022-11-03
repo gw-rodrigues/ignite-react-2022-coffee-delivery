d1187eee237f5f4ae1d418959d4c52538204d1e6
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-03 10:59:37 +0100

    style: header, main and list

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.main`
+  max-width: 1120px;
+  margin: 0 auto;
+  margin-bottom: 9.81rem;
+`
+
+export const CoffeeListContainer = styled.ul`
+  max-width: 1120px;
+  width: 100%;
+  margin: 2.125rem auto;
+
+  list-style: none;
+
+  display: flex;
+  justify-content: space-evenly;
+`
