a90bebd74c8b50b78361a7bd0b48e5e351369797
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-06 12:41:03 +0100

    fix: form container and content

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -2,16 +2,23 @@ import { AddressForm } from './components/AddressForm'
 import { PaymentForm } from './components/PaymentForm'
 import { OrderReview } from './components/OrderReview'
 import { CheckoutContainer, FormContainer } from './styles'
+import { HtmlHTMLAttributes, ReactHTMLElement } from 'react'
 
 export function Checkout() {
+  function handleFormSubmit(e: any) {
+    e.preventDefault()
+  }
+
   return (
     <CheckoutContainer>
-      <FormContainer>
-        <h3>Complete seu pedido</h3>
-        <AddressForm />
-        <PaymentForm />
-      </FormContainer>
-      <OrderReview />
+      <form onSubmit={handleFormSubmit}>
+        <FormContainer>
+          <h3>Complete seu pedido</h3>
+          <AddressForm />
+          <PaymentForm />
+        </FormContainer>
+        <OrderReview />
+      </form>
     </CheckoutContainer>
   )
 }
