c270f87c69629fa7bc7f83935af79ef5839ddd96
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-06 16:05:30 +0100

    feat: nav link to logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,14 @@ import { Navbar } from '../Navbar'
 
 import { HeaderContainer } from './styles'
 import LogoImg from '../../assets/Logo.svg'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <a href="/">
+      <NavLink to="/">
         <img src={LogoImg} alt="Coffee Cup - Coffee delivery" />
-      </a>
+      </NavLink>
       <Navbar />
     </HeaderContainer>
   )
