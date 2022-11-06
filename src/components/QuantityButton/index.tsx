a7aa000a12be13030910ecbea48df41ca4a200e8
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-06 15:45:37 +0100

    feat: handle context update product card quantity

--- src/components/QuantityButton/index.tsx
+++ src/components/QuantityButton/index.tsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from 'react'
 import { QuantitySelect } from './styles'
 
-export function QuantityButton() {
+interface IQuantityButtonProps {
+  defaultValue: number
+  handleUpdateQuantity: (quantity: number) => void
+}
+
+export function QuantityButton({
+  defaultValue,
+  handleUpdateQuantity,
+}: IQuantityButtonProps) {
+  const [quantity, setQuantity] = useState(defaultValue)
+
+  function handleAddProductQuantity() {
+    if (quantity === 9) return
+    setQuantity((prev) => prev + 1)
+  }
+
+  function handleRemoveProductQuantity() {
+    if (quantity === 1) return
+    setQuantity((prev) => prev - 1)
+  }
+
+  useEffect(() => {
+    handleUpdateQuantity(quantity)
+  }, [quantity])
+
   return (
     <QuantitySelect>
-      <button>-</button>
-      <span>1</span>
-      <button>+</button>
+      <button type="button" onClick={handleRemoveProductQuantity}>
+        -
+      </button>
+      <span>{quantity}</span>
+      <button type="button" onClick={handleAddProductQuantity}>
+        +
+      </button>
     </QuantitySelect>
   )
 }
