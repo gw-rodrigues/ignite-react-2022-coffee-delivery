2f28cdbd495944154ce252138b30dbeb33b28b7d
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-06 13:32:57 +0100

    feat: cart quantity from context

--- src/components/Navbar/Cart/index.tsx
+++ src/components/Navbar/Cart/index.tsx
@@ -1,10 +1,21 @@
 import { ShoppingCart } from 'phosphor-react'
+import { useContext, useEffect } from 'react'
+import { CartContext } from '../../../contexts/cartContext'
 import { CartContainer, CartItemsCount } from './styles'
 
 export function Cart() {
+  const { cart } = useContext(CartContext)
+  let cartTotalItems = 0
+
+  useEffect(() => {
+    cartTotalItems = cart.reduce((total, item) => total + item.quantity, 0)
+  }, [cart])
+
   return (
     <CartContainer>
-      <CartItemsCount>3</CartItemsCount>
+      {cartTotalItems ? (
+        <CartItemsCount>{cartTotalItems}</CartItemsCount>
+      ) : null}
       <ShoppingCart size={22} weight="fill" />
     </CartContainer>
   )
