de733a517bc30c9281c7e2021e1dc1050e574628
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-07 12:54:38 +0100

    feat: use form context and register

--- src/pages/Checkout/components/Input/index.tsx
+++ src/pages/Checkout/components/Input/index.tsx
@@ -1,22 +1,26 @@
-import { InputHTMLAttributes, ReactHTMLElement } from 'react'
+import { InputHTMLAttributes } from 'react'
+import { useFormContext } from 'react-hook-form'
 import { InputElement, InputOptionalElement } from './styles'
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  id: string
   inputSize?: 'sm' | 'md' | 'lg'
   optional?: boolean
 }
 
 export function Input({
+  id,
   inputSize = 'lg',
   optional = false,
   ...rest
 }: IInputProps) {
+  const { register } = useFormContext()
   return optional ? (
     <InputOptionalElement inputSize={inputSize}>
-      <input {...rest} />
+      <input {...rest} {...register(id)} />
       <span>Opcional</span>
     </InputOptionalElement>
   ) : (
-    <InputElement {...rest} inputSize={inputSize} />
+    <InputElement {...rest} inputSize={inputSize} {...register(id)} />
   )
 }
