47e84c35d7b2907867f72329c3e08c37e4acd902
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-07 12:54:25 +0100

    feat: hook-form and zod resolver

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,23 +1,67 @@
+import { FormProvider, FormState, useForm, useFormState } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import { AddressForm } from './components/AddressForm'
 import { PaymentForm } from './components/PaymentForm'
 import { OrderReview } from './components/OrderReview'
 import { CheckoutContainer, FormContainer } from './styles'
-import { HtmlHTMLAttributes, ReactHTMLElement } from 'react'
+
+const orderFormValidationSchema = zod.object({
+  cep: zod.number(),
+  // .min(8, 'CEP no mínimo 8 números.')
+  // .max(99999999, 'CEP no máximo 8 números.'),
+  street: zod.string().min(1, 'Endereço é obrigatória.'),
+  houseNumber: zod.number().min(1, 'Número da casa é obrigatória.'),
+  houseComplement: zod.string().optional(),
+  district: zod.string().min(1, 'Bairro é obrigatória.'),
+  city: zod.string().min(1, 'Cidade é obrigatória.'),
+  uf: zod
+    .number()
+    .min(2, 'UF no mínimo 2 números.')
+    .max(2, 'UF no máximo 2 números.'),
+})
+
+type TOrderFormData = zod.infer<typeof orderFormValidationSchema>
 
 export function Checkout() {
-  function handleFormSubmit(e: any) {
-    e.preventDefault()
+  const orderForm = useForm<TOrderFormData>({
+    resolver: zodResolver(orderFormValidationSchema),
+    // defaultValues: {
+    //   cep: 0,
+    //   street: '',
+    //   houseNumber: 0,
+    //   houseComplement: '',
+    //   district: '',
+    //   city: '',
+    //   uf: 0,
+    // },
+  })
+
+  const {
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = orderForm
+
+  console.log(errors)
+
+  function handleFormSubmit(data: TOrderFormData) {
+    console.log(data)
   }
 
   return (
     <CheckoutContainer>
-      <form onSubmit={handleFormSubmit}>
+      <form onSubmit={handleSubmit(handleFormSubmit)} action="">
         <FormContainer>
           <h3>Complete seu pedido</h3>
-          <AddressForm />
+          <FormProvider {...orderForm}>
+            <AddressForm />
+          </FormProvider>
           <PaymentForm />
         </FormContainer>
         <OrderReview />
+        <button type="submit">teste</button>
       </form>
     </CheckoutContainer>
   )
