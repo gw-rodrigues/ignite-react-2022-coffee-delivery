b93ed6950dcda2ec44ffea332d025ebe76549860
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-09 11:06:13 +0100

    feat: radio inputs, register and type

--- src/pages/Checkout/components/PaymentForm/index.tsx
+++ src/pages/Checkout/components/PaymentForm/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { useFormContext } from 'react-hook-form'
 import { Bank, CreditCard, CurrencyDollar, Money } from 'phosphor-react'
 import {
   PaymentMethodContainer,
@@ -5,7 +7,19 @@ import {
   PaymentMethodSelectContainer,
 } from './styled'
 
+import { EPaymentMethods } from '../..'
+
+type TPaymentMethods = keyof typeof EPaymentMethods | undefined
+
 export function PaymentForm() {
+  const { register } = useFormContext()
+
+  const [paymentMethod, setPaymentMethod] = useState<TPaymentMethods>()
+
+  const isDebit = paymentMethod === 'debit' ? true : false
+  const isCredit = paymentMethod === 'credit' ? true : false
+  const isMoney = paymentMethod === 'money' ? true : false
+
   return (
     <PaymentMethodContainer>
       <header>
@@ -17,18 +31,46 @@ export function PaymentForm() {
           </p>
         </div>
       </header>
+
       <PaymentMethodSelectContainer>
-        <PaymentMethodSelectButton type="button">
-          <CreditCard size={16} />
-          Cartão de crédito
+        <PaymentMethodSelectButton selected={isCredit}>
+          <input
+            {...register('paymentMethod')}
+            id="credit"
+            type="radio"
+            value="credit"
+            onChange={() => setPaymentMethod('credit')}
+          />
+          <label htmlFor="credit">
+            <CreditCard size={16} />
+            Cartão de crédito
+          </label>
         </PaymentMethodSelectButton>
-        <PaymentMethodSelectButton type="button">
-          <Bank size={16} />
-          cartão de débito
+        <PaymentMethodSelectButton selected={isDebit}>
+          <input
+            {...register('paymentMethod')}
+            id="debit"
+            type="radio"
+            value="debit"
+            onChange={() => setPaymentMethod('debit')}
+          />
+          <label htmlFor="debit">
+            <Bank size={16} />
+            cartão de débito
+          </label>
         </PaymentMethodSelectButton>
-        <PaymentMethodSelectButton type="button" selected disabled>
-          <Money size={16} />
-          dinheiro
+        <PaymentMethodSelectButton selected={isMoney}>
+          <input
+            {...register('paymentMethod')}
+            id="money"
+            type="radio"
+            value="money"
+            onChange={() => setPaymentMethod('money')}
+          />
+          <label htmlFor="money">
+            <Money size={16} />
+            dinheiro
+          </label>
         </PaymentMethodSelectButton>
       </PaymentMethodSelectContainer>
     </PaymentMethodContainer>
