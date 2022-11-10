fd591cb18da479f275e8187d0ba940600f0cae7b
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-10 12:16:23 +0100

    feat: list UF

--- src/pages/Checkout/components/AddressForm/index.tsx
+++ src/pages/Checkout/components/AddressForm/index.tsx
@@ -1,6 +1,37 @@
 import { MapPinLine } from 'phosphor-react'
 import { Input } from '../../components/Input'
+import { Select } from '../Select'
 import { AddressContainer } from './styles'
+
+const listUf = [
+  { name: 'Rondônia', uf: 'RO' },
+  { name: 'Acre', uf: '	AC' },
+  { name: 'Amazonas', uf: '	AM' },
+  { name: 'Roraima', uf: '	RR' },
+  { name: 'Pará', uf: '	PA' },
+  { name: 'Amapá', uf: '	AP' },
+  { name: 'Tocantins', uf: '	TO' },
+  { name: 'Maranhão', uf: '	MA' },
+  { name: 'Piauí', uf: '	PI' },
+  { name: 'Ceará', uf: '	CE' },
+  { name: 'Rio Grande do Norte', uf: '	RN' },
+  { name: 'Paraíba', uf: '	PB' },
+  { name: 'Pernambuco', uf: '	PE' },
+  { name: 'Alagoas', uf: '	AL' },
+  { name: 'Sergipe', uf: '	SE' },
+  { name: 'Bahia', uf: '	BA' },
+  { name: 'Minas Gerais', uf: '	MG' },
+  { name: 'Espírito Santo', uf: '	ES' },
+  { name: 'Rio de Janeiro', uf: '	RJ' },
+  { name: 'São Paulo', uf: '	SP' },
+  { name: 'Paraná', uf: '	PR' },
+  { name: 'Santa Catarina', uf: '	SC' },
+  { name: 'Rio Grande do Sul (*)', uf: '	RS' },
+  { name: 'Mato Grosso do Sul', uf: '	MS' },
+  { name: 'Mato Grosso', uf: '	MT' },
+  { name: 'Goiás', uf: '	GO' },
+  { name: 'Distrito Federal', uf: '	DF' },
+]
 
 export function AddressForm() {
   return (
@@ -47,15 +78,17 @@ export function AddressForm() {
             required
           />
           <Input id="city" type="text" placeholder="Cidade" required />
-          <Input
-            id="uf"
-            type="number"
-            placeholder="UF"
-            inputSize="sm"
-            min={1}
-            max={26}
-            required
-          />
+
+          <Select id="uf" defaultValue="" inputSize="sm">
+            <option value="" disabled>
+              UF
+            </option>
+            {listUf.map(({ uf }) => (
+              <option key={uf} value={uf}>
+                {uf}
+              </option>
+            ))}
+          </Select>
         </div>
       </section>
     </AddressContainer>
