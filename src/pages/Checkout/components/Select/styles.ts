06966ed7e51a889790e28ae9de72808558067d42
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-11 10:12:29 +0100

    style: change select color is value selected

--- src/pages/Checkout/components/Select/styles.ts
+++ src/pages/Checkout/components/Select/styles.ts
@@ -8,12 +8,13 @@ const SELECT_SIZE_LIST = {
 
 interface ISelectSize {
   inputSize: keyof typeof SELECT_SIZE_LIST
+  valueSelected: boolean
 }
 
 export const SelectElement = styled.select<ISelectSize>`
   max-width: ${(props) => props.theme[SELECT_SIZE_LIST[props.inputSize]]};
-
-  &:disabled {
-    color: ${(props) => props.theme['gray-600']} !important;
-  }
+  color: ${(props) =>
+    props.valueSelected
+      ? props.theme['gray-700']
+      : props.theme['gray-600']} !important;
 `
