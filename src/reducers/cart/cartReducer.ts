a12a5a945c8ae140d56f7d458b2131704edd3e88
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-14 10:16:46 +0100

    feat: reducer actions add, update and remove

---
+++ src/reducers/cart/cartReducer.ts
@@ -0,0 +1,37 @@
+import { string } from 'zod'
+import { TCartProduct } from '../../contexts/cartContext'
+import { EActionsTypes } from './actions'
+
+interface ICartState {
+  cart: TCartProduct[]
+}
+
+interface IActionReceived {
+  type: keyof typeof EActionsTypes
+  payload: any
+}
+
+export function cartReducer(state: ICartState, action: IActionReceived) {
+  switch (action.type) {
+    case EActionsTypes.ADD_PRODUCT:
+      return { ...state, cart: [...state.cart, action.payload.newProduct] }
+
+    case EActionsTypes.UPDATE_PRODUCT_QUANTITY:
+      return {
+        cart: state.cart.map((item) => {
+          if (item.id === action.payload.id) {
+            item.orderQuantity = action.payload.orderQuantity
+          }
+          return item
+        }),
+      }
+
+    case EActionsTypes.REMOVE_PRODUCT:
+      return {
+        cart: state.cart.filter((item) => item.id !== action.payload.id),
+      }
+
+    default:
+      return state
+  }
+}
