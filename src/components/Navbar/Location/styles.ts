12309c9974fc4c258741bfe76b9ae100f2b5a51d
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-15 10:27:06 +0100

    style: modal and button

--- src/components/Navbar/Location/styles.ts
+++ src/components/Navbar/Location/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
-import { MapPin } from 'phosphor-react'
 
-export const LocalizationContainer = styled.button`
+export const LocalizationContainer = styled.div`
+  display: relative;
+`
+
+export const LocalizationButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -24,5 +27,30 @@ export const LocalizationContainer = styled.button`
 
   svg {
     fill: ${(props) => props.theme['purple-500']};
+  }
+`
+
+interface ILocationListIsHiddenProps {
+  hidden: boolean
+}
+
+export const LocalizationList = styled.div<ILocationListIsHiddenProps>`
+  max-width: 250px;
+  min-height: 100px;
+  position: absolute;
+  right: 200px;
+  margin-top: 0.5rem;
+  padding: 0.5rem;
+  border-radius: 6px;
+  background: ${(props) => props.theme['gray-200']};
+
+  display: ${(props) => (props.hidden ? 'none' : 'grid')};
+  border: 1px solid ${(props) => (props.hidden ? 'transparent' : props.theme['purple-100'])};
+
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+
+  button {
+    font-size: 0.6rem;
   }
 `
