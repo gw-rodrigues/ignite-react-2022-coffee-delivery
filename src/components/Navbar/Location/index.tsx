e3210d85b2e031d63c93345cfbf89e189e7917cc
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-15 10:26:50 +0100

    feat: add location list and select modal

--- src/components/Navbar/Location/index.tsx
+++ src/components/Navbar/Location/index.tsx
@@ -1,11 +1,38 @@
 import { MapPin } from 'phosphor-react'
-import { LocalizationContainer } from './styles'
+import { MouseEvent, useState } from 'react'
+import {
+  LocalizationButton,
+  LocalizationContainer,
+  LocalizationList,
+} from './styles'
 
 export function Location() {
+  const [isHidden, setIsHidden] = useState(false)
+  const [location, setLocation] = useState('Porto Alegre, RS')
+
+  function handleSelectShopLocation(e: MouseEvent<HTMLButtonElement>) {
+    const { innerText }: any = e.target
+    setLocation(innerText)
+    setIsHidden(true)
+  }
   return (
     <LocalizationContainer>
-      <MapPin size={22} weight="fill" />
-      <span>Porto Alegre, RS</span>
+      <LocalizationButton
+        onClick={() => {
+          setIsHidden(!isHidden)
+        }}
+      >
+        <MapPin size={22} weight="fill" />
+        <span>{location}</span>
+      </LocalizationButton>
+      <LocalizationList hidden={isHidden}>
+        <button onClick={handleSelectShopLocation}>Santa Catarina, SC</button>
+        <button onClick={handleSelectShopLocation}>São Paulo, SP</button>
+        <button onClick={handleSelectShopLocation}>Bahia, MG</button>
+        <button onClick={handleSelectShopLocation}>Tocantins, TO</button>
+        <button onClick={handleSelectShopLocation}>Goiás, GO</button>
+        <button onClick={handleSelectShopLocation}>Rio de Janeiro, RJ</button>
+      </LocalizationList>
     </LocalizationContainer>
   )
 }
